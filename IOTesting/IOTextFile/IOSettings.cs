01296e8cbbe161b1da9050945504ecb2785529ba
Румен Драганов <r.draganov@respect93.com>
2015-09-06 21:36:04 +0300

    Модул 08 Входно-изходни операции с файлове Видео 3-4-5

---
+++ IOTesting/IOTextFile/IOSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace IOTextFile
+{
+	public class IOSettings
+	{
+		private STable _stable;
+		public IOSettings (STable stable)
+		{
+			_stable = stable;
+		}
+
+		public string getPath()
+		{
+			string _path=System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"test.txt");
+			string _user = Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData);
+			Console.WriteLine (_user);
+			string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
+			Console.WriteLine (_desktop);
+			return _path;
+		}
+
+
+		public bool save()
+		{
+			try{
+				string _temp="";
+				_temp=string.Join("\r\n",_stable.stable);
+				//запис на текстов файл
+				System.IO.File.WriteAllText("d:\\aula\\test.txt",_temp);
+				return true;
+			}catch{
+			}
+			return false;
+		}
+
+		public bool open()
+		{
+			try{
+				string _temp = "", filePath=getPath();
+				if (System.IO.File.Exists(filePath))
+					_temp=System.IO.File.ReadAllText(filePath);
+				else {
+					Console.WriteLine("Пътят не е намерен.");
+					return false;}
+
+				string[] _table=_temp.Replace("\r","").Split('\n');
+				for (int i=0; i<_table.Length; i++)
+				{
+					_stable.stable[i]=_table[i];
+				}
+				return true;
+			}catch{
+			}
+			return false;
+
+		}
+
+
+	}
+}
+
---
+++ IOTesting/IOTextFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IOTextFile
+{
+	class MainClass
+	{
+		public static STable _STable = new STable();
+		public static IOSettings _io=new IOSettings(_STable);
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (_STable.stable[0]);
+
+//			if (_io.save()) {
+//				Console.WriteLine ("Записът е успешен");
+//			} else {
+//				Console.WriteLine ("Записът НЕ е успешен");
+//			}
+			if (_io.open()) {
+				Console.WriteLine ("Четенето е успешно");
+			} else {
+				Console.WriteLine ("Четенето НЕ е успешно");
+			}
+			Console.WriteLine (_STable.stable[2]);
+		}
+	}
+}
