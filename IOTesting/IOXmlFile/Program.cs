a3b90f43bf80d83916710d48aa7fb8f71fcdcb99
Румен Драганов <r.draganov@respect93.com>
2015-09-06 23:49:34 +0300

    Модул 08 Четене от XML файл

---
+++ IOTesting/IOXmlFile/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace IOXmlFile
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string value = "", property = "";
+			string path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+
+			//Четене от Xml файл
+			using (XmlReader reader = XmlReader.Create (path)) {
+				while (reader.Read ()) {
+					switch (reader.Name) {
+					case "row":
+						property = reader ["property"];
+						value = reader.ReadInnerXml ();
+						Console.WriteLine ("value = "+ value);
+						Console.WriteLine ("property = "+ property);
+						break;
+					case "simplerow":
+						//property = reader ["property"];
+						value = reader.ReadInnerXml ();
+						Console.WriteLine ("value = "+ value);
+						//Console.WriteLine ("property = "+ property);
+						break;
+					}
+				}
+			}
+
+		
+			Console.ReadKey();
+
+			//Запис в Xml файл
+//			using (XmlWriter writer = XmlWriter.Create (path)) {
+//				writer.WriteStartDocument ();
+//
+//				writer.WriteStartElement ("settings");
+//
+//				writer.WriteStartElement ("row");
+//
+//				writer.WriteAttributeString ("property", property);
+//				writer.WriteString (value);
+//
+//				writer.WriteEndElement ();
+//
+//				writer.WriteEndElement();
+//
+//				writer.WriteEndDocument ();
+//			}
+//			XDocument document = XDocument.Load (path);
+//			document.Save (path);
+//
+//			System.Diagnostics.Process.Start (path);
+				
+		}
+	}
+}
